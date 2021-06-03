57ef05a4e951a285240fd8ecf4c04bfe4db0dc2f
samuelssan28 <samuelssan28@gmail.com>
2021-06-02 23:01:43 -0300

    alterando arquivo de teste

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learning about CI and CD with Travis CI/i);
   expect(linkElement).toBeInTheDocument();
 });
